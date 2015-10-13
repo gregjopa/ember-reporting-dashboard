02fcd8a25ca8d230f0c67b950c62aaf300712b4f
Greg Jopa <gj870q@yp.com>
2015-10-13 08:21:33 -0700

    Add bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,18 +6,15 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
+  var fontFile = 'bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular';
+  app.import(fontFile + '.eot', { destDir: 'fonts' });
+  app.import(fontFile + '.ttf', { destDir: 'fonts' });
+  app.import(fontFile + '.svg', { destDir: 'fonts' });
+  app.import(fontFile + '.woff', { destDir: 'fonts' });
+  app.import(fontFile + '.woff2', { destDir: 'fonts' });
 
   return app.toTree();
 };

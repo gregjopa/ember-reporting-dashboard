f7a402504ac90ee276fd7152f9615ee776642a4a
Greg Jopa <gj870q@yp.com>
2015-10-13 08:31:06 -0700

    Add routes

--- app/router.js
+++ app/router.js
@@ -5,7 +5,18 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+Router.map(function () {
+
+  this.route('report-comparison', { path: '/comparison' }, function () {
+    this.route('results', { path: ':repos' });
+  });
+
+  this.route('report-details', { path: '/details' }, function () {
+    this.route('results');
+  });
+
+  this.route('error');
+
 });
 
 export default Router;

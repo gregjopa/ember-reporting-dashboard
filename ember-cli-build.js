1cfb9b4e1202b0368911c3f305624341fa0982b6
Greg Jopa <grjopa@gmail.com>
2015-12-01 13:02:02 -0800

    Upgrade to ember-cli 1.13.13

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var funnel = require('broccoli-funnel');
 
 module.exports = function (defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
+  var app = new EmberApp(defaults);
 
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');

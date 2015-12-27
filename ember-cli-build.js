9b82bdfefde549a2b235ea1e4d64219847e1d43f
Greg Jopa <grjopa@gmail.com>
2015-12-27 10:08:28 -0800

    Only load necessary bootstrap and highcharts js files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,10 +3,18 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var funnel = require('broccoli-funnel');
 
 module.exports = function (defaults) {
-  var app = new EmberApp(defaults);
+  var app = new EmberApp(defaults, {
+    emberHighCharts: {
+      includeHighCharts: true,
+      includeHighStock: false,
+      includeHighMaps: false,
+      includeHighChartsMore: false
+    }
+  });
 
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import('bower_components/bootstrap/js/transition.js');
+  app.import('bower_components/bootstrap/js/collapse.js');
 
   var fonts = new funnel('bower_components/bootstrap/fonts', {
     srcDir: '/',
